dc8b5a3bb34964bf5ece216b19b9aff6fd23a451
hojkas <ivka.strnadova@gmail.com>
2022-09-13 11:07:12 +0200

    feat: main and argparse setup
    outline of classes
    
    feat: main.py with argument parsing
    
    fix: prettier code and readme

---
+++ day_constructor.py
@@ -0,0 +1,15 @@
+# WHAT IT NEEDS
+# day, year, input
+
+class ConstructionException(Exception):
+    pass
+
+
+def fetch_input():
+    pass
+
+
+class DayConstructor:
+    @staticmethod
+    def construct(day, year, auto_input):
+        fetch_input()
