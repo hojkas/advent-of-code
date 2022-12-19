7d00f04dc7beee964c87e4a3698fd189d6bc4745
hojkas <ivka.strnadova@gmail.com>
2022-12-19 23:31:59 +0100

    feat: abstract day

---
+++ abstract_day.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractDay(ABC):
+    @abstractmethod
+    def add_input_loader(self, input_loader):
+        pass
+
+    @abstractmethod
+    def use_debug(self, use_debug=True):
+        pass
+
+    @abstractmethod
+    def run_part_one(self):
+        pass
+
+    @abstractmethod
+    def run_part_two(self):
+        pass
