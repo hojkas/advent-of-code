0f462f4a58716b72aab3668d9ee2a1d71052e874
hojkas <ivka.strnadova@gmail.com>
2022-12-20 13:21:34 +0100

    fix: improved template and input loader

--- input_loader.py
+++ input_loader.py
@@ -8,25 +8,21 @@ class InputLoader:
             raise RunException('Input file with name ' + str(filepath) + ' does not exist.')
         self.filepath = filepath
 
-    def _load_input_as_generator(self, separator, retype_int=False):
-        pass
+    def load_input_as_generator(self, item_separator, retype_int=False):
+        raise RunException('Load input function not yet implemented')
 
-    def _load_input(self, separator, retype_int=False):
+    def load_input_array(self, item_separator, retype_item_to_int=False):
         with open(self.filepath, 'r') as f:
-            full_input = f.read().split(separator)
-        if retype_int:
+            full_input = f.read().split(item_separator)
+        if retype_item_to_int:
             return [int(x) for x in full_input]
         else:
             return full_input
 
-    def load_input_string_array(self, separator='\n'):
-        return self._load_input(separator)
-
-    def load_input_int_array(self, separator='\n'):
-        return self._load_input(separator, True)
-
-    def load_input_string_array_as_generator(self, separator='\n'):
-        pass
-
-    def load_input_int_array_as_generator(self, separator='\n'):
-        pass
+    def load_input_array_of_array(self, subarray_separator, item_separator, retype_item_to_int=False):
+        with open(self.filepath, 'r') as f:
+            full_input = f.read().split(subarray_separator)
+        if retype_item_to_int:
+            return [[int(x) for x in y.split(item_separator)] for y in full_input]
+        else:
+            return [y.split(item_separator) for y in full_input]
