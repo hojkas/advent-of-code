98d0af7f3e6406a9f442a7077a4886ef2c70ea4e
hojkas <ivka.strnadova@gmail.com>
2023-12-01 13:07:03 +0100

    feat: new year

---
+++ year2023/days/day1.py
@@ -0,0 +1,17 @@
+from typing import Union
+from lib.abstract_day import AbstractDay
+from helpers import InputLoader
+
+
+class DayRunner(AbstractDay):
+    def __init__(self):
+        self.input_loader: Union[InputLoader, None] = None
+
+    def add_input_loader(self, input_loader):
+        self.input_loader = input_loader
+
+    def run_part_one(self):
+        return "---"
+
+    def run_part_two(self):
+        return "---"
