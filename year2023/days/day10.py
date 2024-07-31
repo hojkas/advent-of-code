5ebf2455435b0b5754d0c9c6f2648bbdd96daba7
Iveta Strnadová <iveta.strnadova@firma.seznam.cz>
2024-07-31 16:10:10 +0200

    feat: day 10, part 2

--- year2023/days/day10.py
+++ year2023/days/day10.py
@@ -20,12 +20,21 @@ class Direction(Enum):
     RIGHT = (0, 1)
 
     @property
-    def opposite(self):
+    def opposite(self) -> 'Direction':
         return {
             Direction.UP: Direction.DOWN,
             Direction.DOWN: Direction.UP,
             Direction.LEFT: Direction.RIGHT,
             Direction.RIGHT: Direction.LEFT
+        }[self]
+
+    @property
+    def next_clockwise(self) -> 'Direction':
+        return {
+            Direction.UP: Direction.RIGHT,
+            Direction.DOWN: Direction.LEFT,
+            Direction.LEFT: Direction.UP,
+            Direction.RIGHT: Direction.DOWN
         }[self]
 
 
@@ -61,11 +70,19 @@ PIPE_TYPE_TRANSLATION = {
 }
 
 
-class PipeLocationtype(Enum):
+class PipeLocationType(Enum):
     LOOP = 0
-    INSIDE = 1
-    OUTSIDE = 2
+    LEFT_HANDED_AREA = 1
+    RIGHT_HANDED_AREA = 2
     UNDECIDED = 3
+
+    @property
+    def opposite(self) -> 'PipeLocationType':
+        if self == PipeLocationType.RIGHT_HANDED_AREA:
+            return PipeLocationType.LEFT_HANDED_AREA
+        if self == PipeLocationType.LEFT_HANDED_AREA:
+            return PipeLocationType.RIGHT_HANDED_AREA
+        raise RunException(f"Cannot deterine opposite of location type {self.name}")
 
 
 class Pipe:
@@ -76,10 +93,10 @@ class Pipe:
         self.neighbours: dict[Direction, Optional['Pipe']] = {direction: None for direction in Direction}
         self.is_start = True if representation == "S" else False
         self.is_empty = self.type == PipeType.NONE
-        self.location = PipeLocationtype.UNDECIDED
+        self.location = PipeLocationType.UNDECIDED
 
     def __repr__(self):
-        return f"Pipe({self.type}, {self.row}, {self.col})"
+        return f"Pipe({self.type}, {self.row}, {self.col}, {self.location})"
 
     def goes_in_direction(self, direction: Direction) -> bool:
         return direction in self.get_directions()
@@ -107,9 +124,37 @@ class Pipe:
         return neighbour
 
 
+class OrientedPipe:
+    def __init__(self, pipe: Pipe, entry_direction: Direction, exit_direction: Direction) -> None:
+        self.pipe = pipe
+        self.entry_direction = entry_direction
+        self.exit_direction = exit_direction
+
+    def __repr__(self):
+        return f"{self.pipe} <{self.entry_direction} {self.exit_direction}>"
+
+    def get_left_hand_directions(self):
+        directions = []
+        curr_direction = self.entry_direction.next_clockwise
+        while curr_direction != self.exit_direction:
+            directions.append(curr_direction)
+            curr_direction = curr_direction.next_clockwise
+        return directions
+
+    def get_right_hand_directions(self):
+        directions = []
+        curr_direction = self.exit_direction.next_clockwise
+        while curr_direction != self.entry_direction:
+            directions.append(curr_direction)
+            curr_direction = curr_direction.next_clockwise
+        return directions
+
+
 class PipeMap:
     def __init__(self, input_lines: list[str]) -> None:
-        self.pipes = []
+        self.pipes: list[list[Pipe]] = []
+        self.loop: list[OrientedPipe] = []
+
         starting_node = None
         for row, line in enumerate(input_lines):
             pipe_row = []
@@ -163,6 +208,49 @@ class PipeMap:
         if starting_node.type == PipeType.UNDECIDED:
             raise RunException("Cannot decide for PipeType UNDECIDED")
 
+    def determine_inner_location_type(self) -> PipeLocationType:
+        # check the first line
+        for pipe in self.pipes[0]:
+            if pipe.location in [PipeLocationType.LEFT_HANDED_AREA, PipeLocationType.RIGHT_HANDED_AREA]:
+                return pipe.location.opposite
+        # and the last
+        for pipe in self.pipes[-1]:
+            if pipe.location in [PipeLocationType.LEFT_HANDED_AREA, PipeLocationType.RIGHT_HANDED_AREA]:
+                return pipe.location.opposite
+        # and first and last columns
+        for pipe_line in self.pipes:
+            if pipe_line[0].location in [PipeLocationType.LEFT_HANDED_AREA, PipeLocationType.RIGHT_HANDED_AREA]:
+                return pipe_line[0].location.opposite
+            if pipe_line[-1].location in [PipeLocationType.LEFT_HANDED_AREA, PipeLocationType.RIGHT_HANDED_AREA]:
+                return pipe_line[-1].location.opposite
+        # finally, if none of the edge fields had location (hence the full edge is a loop), pick the only area present
+        for pipe_line in self.pipes[1:-1]:
+            for pipe in pipe_line[1:-1]:
+                if pipe.location in [PipeLocationType.LEFT_HANDED_AREA, PipeLocationType.RIGHT_HANDED_AREA]:
+                    return pipe.location
+
+        raise RunException("Cannot determine inner location type")
+
+    def count_pipes_with_location_type(self, location_type: PipeLocationType) -> int:
+        count = 0
+        for pipe_line in self.pipes:
+            for pipe in pipe_line:
+                if pipe.location == location_type:
+                    count += 1
+        return count
+
+    def print(self):
+        for pipe_line in self.pipes:
+            for pipe in pipe_line:
+                char = {
+                    PipeLocationType.LOOP: "#",
+                    PipeLocationType.UNDECIDED: "?",
+                    PipeLocationType.LEFT_HANDED_AREA: "o",
+                    PipeLocationType.RIGHT_HANDED_AREA: "i",
+                }[pipe.location]
+                print(char, end="")
+            print()
+
 
 class DayRunner(AbstractDay):
     def __init__(self):
@@ -207,4 +295,76 @@ def run_part_one(pipe_map: PipeMap) -> int:
 
 
 def run_part_two(pipe_map: PipeMap) -> int:
-    pass
+    _mark_loop_pipes(pipe_map)
+    _flood_fill_map(pipe_map)
+    inner_location_type = pipe_map.determine_inner_location_type()
+    return pipe_map.count_pipes_with_location_type(inner_location_type)
+
+
+def _mark_loop_pipes(pipe_map: PipeMap) -> None:
+    starting_pipe = pipe_map.starting_pipe
+    previous_exit_direction, _ = starting_pipe.get_directions()
+    previous_pipe = starting_pipe
+    starting_pipe.location = PipeLocationType.LOOP
+
+    while True:
+        current_entry_direction = previous_exit_direction.opposite
+        current_pipe = previous_pipe.get_neighbour_in_direction(previous_exit_direction)
+        current_exit_direction = current_pipe.get_exit_direction(current_entry_direction)
+
+        if current_pipe == starting_pipe:
+            break
+
+        pipe_map.loop.append(OrientedPipe(current_pipe, current_entry_direction, current_exit_direction))
+        current_pipe.location = PipeLocationType.LOOP
+
+        previous_pipe = current_pipe
+        previous_exit_direction = current_exit_direction
+
+
+def _flood_fill_map(pipe_map: PipeMap) -> None:
+    for oriented_pipe in pipe_map.loop:
+        _flood_fill_from_pipe(oriented_pipe)
+
+
+def _flood_fill_from_pipe(oriented_loop_pipe: OrientedPipe) -> None:
+    left_hand_directions = oriented_loop_pipe.get_left_hand_directions()
+    right_hand_directions = oriented_loop_pipe.get_right_hand_directions()
+
+    for left_direction in left_hand_directions:
+        try:
+            neighbour_in_left_direction = oriented_loop_pipe.pipe.get_neighbour_in_direction(left_direction)
+            _mark_neighbour_and_flood(neighbour_in_left_direction, PipeLocationType.LEFT_HANDED_AREA)
+        except NeighbourOutOfBoundsException:
+            pass
+
+    for right_direction in right_hand_directions:
+        try:
+            neighbour_in_right_direction = oriented_loop_pipe.pipe.get_neighbour_in_direction(right_direction)
+            _mark_neighbour_and_flood(neighbour_in_right_direction, PipeLocationType.RIGHT_HANDED_AREA)
+        except NeighbourOutOfBoundsException:
+            pass
+
+
+def _mark_neighbour_and_flood_old(pipe: Pipe, location_type: PipeLocationType) -> None:
+    if pipe.location != PipeLocationType.UNDECIDED:
+        return  # already has a type, nowhere to flood
+
+    pipe.location = location_type
+    for neighbour in pipe.neighbours.values():
+        if neighbour is not None:
+            _mark_neighbour_and_flood(neighbour, location_type)
+
+
+def _mark_neighbour_and_flood(first_pipe: Pipe, location_type: PipeLocationType) -> None:
+    if first_pipe.location != PipeLocationType.UNDECIDED:
+        return
+
+    pipes_to_flood = [first_pipe]
+
+    while pipes_to_flood:
+        pipe = pipes_to_flood.pop()
+        pipe.location = location_type
+        for neighbour in pipe.neighbours.values():
+            if neighbour is not None and neighbour.location == PipeLocationType.UNDECIDED:
+                pipes_to_flood.append(neighbour)
