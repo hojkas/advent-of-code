cbd61ca4a5bbbd203894cb46e25c026388d73c5b
Iveta Strnadová <iveta.strnadova@firma.seznam.cz>
2024-12-11 15:29:46 +0100

    feat: day5, part two

--- year2024/days/day5.py
+++ year2024/days/day5.py
@@ -32,6 +32,18 @@ class PageOrdering:
             else:
                 self.after_pages[item.after_page].append(item)
 
+    def get_before_rules_filtered(self, page_filter: list[int]) -> dict[int, list[int]]:
+        before_rules = {}
+        for item in self.items:
+            if item.before_page not in page_filter or item.after_page not in page_filter:
+                continue
+
+            if item.before_page not in before_rules:
+                before_rules[item.before_page] = [item.after_page]
+            else:
+                before_rules[item.before_page].append(item.after_page)
+        return before_rules
+
     def get_where_page_is_before(self, page_number: int) -> list[PageOrderItem]:
         return self.before_pages.get(page_number, [])
 
@@ -62,6 +74,39 @@ class PageSetItem:
                     return False
         return True
 
+    def create_correct_order(self, page_ordering: PageOrdering) -> None:
+        applicable_rules = page_ordering.get_before_rules_filtered(self.pages)
+        ordered = []
+        unordered = self.pages
+
+        while len(unordered) > 0:
+            removed_one = False
+            for page_number in unordered:
+                if page_number not in applicable_rules:  # not a key - is not in relationship with any other page to be before it
+                    ordered.append(page_number)
+                    unordered.remove(page_number)
+                    self._cleanse_applicable_rules(page_number, applicable_rules)
+                    removed_one = True
+                    continue
+            if not removed_one:
+                raise RuntimeError("No key was removed. Cyclic dependency detected.")
+
+        self.pages = ordered
+
+    @staticmethod
+    def _cleanse_applicable_rules(removed_number: int, applicable_rules: dict[int, list[int]]) -> None:
+        # remove the dependancy
+        for after_pages in applicable_rules.values():
+            if removed_number in after_pages:
+                after_pages.remove(removed_number)
+        # remove empty entries
+        keys_to_pop = []
+        for key, value in applicable_rules.items():
+            if len(value) == 0:
+                keys_to_pop.append(key)
+        for key in keys_to_pop:
+            applicable_rules.pop(key)
+
 
 class DayRunner(AbstractDay):
     def __init__(self):
@@ -80,7 +125,17 @@ class DayRunner(AbstractDay):
         return result
 
     def run_part_two(self):
-        return "---"
+        page_ordering_raw, page_sets_raw = self.input_loader.load_input_array_of_array(
+            subarray_separator="\n\n", item_separator="\n"
+        )
+        page_ordering = convert_page_ordering(page_ordering_raw)
+        page_sets = convert_page_sets(page_sets_raw)
+        wrongly_ordered_page_sets = [page_set for page_set in page_sets if not page_set.is_valid(page_ordering)]
+        middle_page_sum = 0
+        for page_set in wrongly_ordered_page_sets:
+            page_set.create_correct_order(page_ordering)
+            middle_page_sum += page_set.middle_page
+        return middle_page_sum
 
 
 def convert_page_ordering(page_ordering_raw: list[str]) -> PageOrdering:
